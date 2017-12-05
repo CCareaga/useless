ab281d97fb0e081948cd69f706dc30eab9a6520c
Chris Careaga <careagc@wwu.edu>
2017-12-04 22:31:56 -0800

    working on debugger

--- src/debug.c
+++ src/debug.c
@@ -2,6 +2,16 @@
 #include "ops.h"
 
 #include <stdio.h>
+#include <stdlib.h>
+
+static int cont = 0;
+
+typedef struct b_point {
+    int address;
+    struct b_point *next;
+} bp_t;
+
+bp_t *head = NULL;
 
 // print the stack and the stack pointers
 static void dump_stack(cpu_t *cpu, int *ram) {
@@ -33,9 +43,72 @@ static void dump_cpu(cpu_t *cpu, int *ram) {
     printf("========================\n\n");
 }
 
+// add a new break point to the linked list
+static void add_bp(int addr) {
+    bp_t *new_bp = malloc(sizeof(bp_t));
+    new_bp->address = addr;
+
+    if (!head) 
+        head = new_bp;
+        return;
+
+    bp_t *temp = head;
+
+    while (temp->next) 
+        temp = temp->next;
+
+    temp->next = new_bp;
+
+}
+
+static bp_t *get_bp(int addr) {
+    bp_t *temp = head;
+
+    while (temp && temp->address != addr)
+        temp = temp->next;
+
+    return temp;
+}
+
 void vm_debug(cpu_t *cpu, int *ram, int op_code) {
     dump_cpu(cpu, ram);
-    dump_stack(cpu, ram);
-    char c = getchar();
+    // dump_stack(cpu, ram);
+    char c;
+    int stopped = 1;
+    
+    if (cont) {
+        stopped = 0;
+        
+        if (get_bp(cpu->pc)) {
+            cont = 0;
+            stopped = 1;
+        }
+    }
+
+    else
+        c = getchar();
+
+    while (stopped) {
+        switch (c) {
+            case 'n':
+                stopped = 0;
+                break;
+    
+            case 'c':   
+                printf("continuing");
+                cont = 1;
+                stopped = 0;
+                break;
+            
+            case 'b':
+                add_bp(28);
+                break;
+
+            default:
+                break;
+        }
+
+        c = getchar();
+    }
 }
 
